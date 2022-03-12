48e3655274ec25f0ca8c828424edd24b5095f706
arata <info@aualrxse.com>
2022-03-12 18:12:25 +0900

    cdn zip を利用してインストール不要にした

--- tasks/createApp.js
+++ tasks/createApp.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const fs = require("fs-extra")
+const https = require("https")
+const unzip = require('unzipper');
 const chalk = require("chalk")
 const COL_SUCCEED = chalk.hex("#0BE081")
 const COL_HAPPY = chalk.hex("#0BC9E0")
@@ -15,36 +17,50 @@ const log = (t) => {
   console.clear()
   console.log(texts)
 }
+const success = async (cwd, appName) => {
+  await sleep(1000)
+  log(COL_HAPPY("created - ") + appName)
+  ln()
+  await sleep(500)
+  log(COL_SUCCEED("success - ") + `Created app at ${cwd}/${appName}`)
+  log("Inside that directory, you can run several commands:");
+  ln()
+  log(COL_HAPPY(" npm run dev"));
+  log("   Starts the development server.");
+  ln()
+  log(COL_HAPPY(" npm run build"));
+  log("   Builds the app for production.");
+  ln()
+  log(COL_HAPPY(" npm start"));
+  log("   Runs the built app in production mode.");
+  ln()
+  await sleep(500)
+  log("We suggest that you begin by typing:");
+  ln()
+  log(COL_HAPPY(" cd ") + appName);
+  log(COL_HAPPY(" npm run dev"));
+  ln()
+}
 
 const copy = async (dir, cwd, appName) => {
-  return new Promise(res => {
-    fs.copy(dir, `${cwd}/${appName}`, async (err) => {
-      if (err) res(false)
-      else {
-        await sleep(1000)
-        log(COL_HAPPY("created - ") + appName)
-        ln()
-        await sleep(500)
-        log(COL_SUCCEED("success - ") + `Created app at ${cwd}/${appName}`)
-        log("Inside that directory, you can run several commands:");
-        ln()
-        log(COL_HAPPY(" npm run dev"));
-        log("   Starts the development server.");
-        ln()
-        log(COL_HAPPY(" npm run build"));
-        log("   Builds the app for production.");
-        ln()
-        log(COL_HAPPY(" npm start"));
-        log("   Runs the built app in production mode.");
-        ln()
-        await sleep(500)
-        log("We suggest that you begin by typing:");
-        ln()
-        log(COL_HAPPY(" cd ") + appName);
-        log(COL_HAPPY(" npm run dev"));
-        ln()
-        res(true)
-      }
+  return new Promise(r => {
+    const out = fs.createWriteStream(`${appName}.zip`)
+    https.get("https://cdn.jsdelivr.net/gh/aratius/quick-sand@master/quick-sand.zip", res => {
+      res.pipe(out)
+      res.on("end", () => {
+        const zip = fs.createReadStream(`./${appName}.zip`)
+        zip.pipe(unzip.Extract({ path: "." }))
+          .promise()
+          .then(async () => {
+            fs.rename("./quick-sand", appName)
+            fs.unlink(`./${appName}.zip`)
+            await success(cwd, appName)
+            r(true)
+          })
+          .catch((e) => {
+            r(false)
+          })
+      })
     })
   })
 }
